13c81b8d803655d4ad6d3459a73fdb8501bb2531
Alfredo de Inocenti <alfreddinocenti@gmail.com>
2023-01-28 19:25:53 -0300

    test

--- service_worker.js
+++ service_worker.js
@@ -3,9 +3,9 @@ const STATIC_CACHE = "static";
 
 //Archivos a guardar
 const APP_SHELL = [
-    "https://fonsecafuentes.github.io/fonseca.github.oi/",
     "https://fonsecafuentes.github.io/fonseca.github.oi/index.html",
     "https://fonsecafuentes.github.io/fonseca.github.oi/css/styles.css",
+    "https://fonsecafuentes.github.io/fonseca.github.oi/",
     "https://fonsecafuentes.github.io/fonseca.github.oi/js/index.js",
     "https://fonsecafuentes.github.io/fonseca.github.oi/json/data_user.json",
     "https://fonsecafuentes.github.io/fonseca.github.oi/img/icono.png",
