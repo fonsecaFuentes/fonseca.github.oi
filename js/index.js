015ff0b8c1be05976cf6ec449ec883395b12d40b
Alfredo de Inocenti <alfreddinocenti@gmail.com>
2023-01-28 04:26:06 -0300

    test

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 
 // Configurar service_worker
-let sw_location = "../service_worker.js";
+let sw_location = "service_worker.js";
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register(sw_location)
